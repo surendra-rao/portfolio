1df6ec5b183e84d695977c1c8353e49612d25c92
Surendra Rao Bongu <surendrarao012@gmail.com>
2025-05-07 13:03:49 +0530

    chore: Update resume link in user data

--- data/user-data.js
+++ data/user-data.js
@@ -5,7 +5,7 @@ export const userData = {
   devUsername: "surendra-rao",
   github: 'https://github.com/surendra-rao',
   linkedIn: 'https://www.linkedin.com/in/surendra-rao-bongu',
-  resume: "https://drive.google.com/file/d/1H02YdNpSLZk1fDxf8gLCWJwqhF7YuPWO/view?usp=sharing",
+  resume: "https://drive.google.com/file/d/1XiLH7XCUddiK-vyc34wuiYpv6nSK0FBq/view?usp=sharing",
   skills: ['React', 'NextJS', 'Redux', 'Express', 'NestJS', 'MySql', 'MongoDB', 'Postgres', 'Docker', 'AWS'],
   technical_skills : [
     "Python", "Django", "Django REST Framework", "FastAPI", "MySQL", "MongoDB", "Redis", "Docker", "Git", "HTML", "CSS", "JavaScript", "React.js", "AWS", "Linux", "Swagger",
